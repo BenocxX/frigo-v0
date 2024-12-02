8cc3d10a675ee55c80688e26aab64355e7363bb4
BenocxX <mathis.cote@outlook.com>
2024-12-01 20:06:02 -0500

    feat: add better ui for buy form

--- src/routes/dashboard/buy/+page.svelte
+++ src/routes/dashboard/buy/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import * as Form from '$lib/components/ui/form/index.js';
-  import SuperDebug, { superForm } from 'sveltekit-superforms';
+  import { superForm } from 'sveltekit-superforms';
   import { zodClient } from 'sveltekit-superforms/adapters';
-  import { browser } from '$app/environment';
-  import Button from '$lib/components/ui/button/button.svelte';
   import { newTransactionSchema } from '$lib/components/custom/forms/transactions/schema.js';
   import { makeFormResultToast } from '$lib/utils/toasts.js';
+  import type { Product } from '@prisma/client';
+  import Plus from 'lucide-svelte/icons/plus';
+  import Minus from 'lucide-svelte/icons/minus';
 
   const { data } = $props();
 
@@ -57,26 +58,73 @@
 </script>
 
 <form method="POST" action="?/newTransaction" use:enhance>
-  <Form.Fieldset {form} name="transactionProducts" class="space-y-0">
+  <Form.Fieldset {form} name="transactionProducts" class="mb-4 space-y-0">
     <div class="mb-4">
-      <Form.Legend class="text-base">Produits</Form.Legend>
-      <Form.Description>Sélectionnez les produits que vous souhaitez acheter</Form.Description>
+      <Form.Legend class="text-2xl">Acheter des produits</Form.Legend>
+      <Form.Description>Sélectionnez les produits que vous souhaitez acheter.</Form.Description>
     </div>
-    <div class="space-y-2">
+    <ul role="list" class="mb-4 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
       {#each data.products as product}
-        <div class="space-y-1">
-          <h4>{product.name} - <span>{product.price}$</span></h4>
-          <Button onclick={() => addProduct(product.id)}>+</Button>
-          {#if $formData.transactionProducts.find((tp) => tp.productId === product.id)}
-            <Button onclick={() => removeProduct(product.id)}>-</Button>
-          {/if}
-        </div>
+        {@render addProductButton(product)}
       {/each}
-      <Form.FieldErrors />
-    </div>
+    </ul>
+    <Form.FieldErrors />
   </Form.Fieldset>
   <Form.Button>Acheter</Form.Button>
-  {#if browser}
-    <SuperDebug data={$formData} />
-  {/if}
 </form>
+
+{#snippet addProductButton(product: Product)}
+  {@const quantity =
+    $formData.transactionProducts.find((tp) => tp.productId === product.id)?.quantity ?? 0}
+  <li class="col-span-1 rounded-lg bg-card shadow">
+    <div class="flex w-full items-center justify-between space-x-6 p-6">
+      <div class="flex-1 truncate">
+        <div class="flex items-center space-x-3">
+          <h3 class="truncate text-sm font-medium text-card-foreground">{product.name}</h3>
+          <!-- <span
+            class="inline-flex shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
+          >
+            Admin
+          </span> -->
+        </div>
+        <!-- <p class="mt-1 truncate text-sm text-gray-500">Regional Paradigm Technician</p> -->
+      </div>
+      <!-- <img
+        class="size-10 shrink-0 rounded-full dark:bg-muted"
+        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+        alt=""
+      /> -->
+    </div>
+    <div class="flex border-t">
+      <div
+        class="flex flex-1 rounded-bl-lg border-r transition-colors only:rounded-br-lg only:border-r-0 hover:bg-green-100 dark:hover:bg-green-950/40"
+      >
+        <button
+          type="button"
+          onclick={() => addProduct(product.id)}
+          class="flex flex-1 items-center justify-center gap-2 py-4 text-green-600"
+        >
+          <Plus />
+          Ajouter
+          {#if quantity > 0}
+            <span>({quantity})</span>
+          {/if}
+        </button>
+      </div>
+      {#if quantity > 0}
+        <div
+          class={'flex flex-1 rounded-br-lg border-r transition-colors hover:bg-red-100 dark:hover:bg-red-950/40'}
+        >
+          <button
+            type="button"
+            onclick={() => removeProduct(product.id)}
+            class="flex flex-1 items-center justify-center gap-2 py-4 text-red-600"
+          >
+            <Minus />
+            Retirer
+          </button>
+        </div>
+      {/if}
+    </div>
+  </li>
+{/snippet}
