1b23d3d51697c40c3f00d1828ac98a65b3f3179a
BenocxX <mathis.cote@outlook.com>
2024-12-01 21:45:40 -0500

    chore: format all

--- src/lib/components/ui/table/table-row.svelte
+++ src/lib/components/ui/table/table-row.svelte
@@ -2,6 +2,7 @@
   import type { HTMLAttributes } from 'svelte/elements';
   import type { WithElementRef } from 'bits-ui';
   import { cn } from '$lib/utils.js';
+  import { fade } from 'svelte/transition';
 
   let {
     ref = $bindable(null),
@@ -17,6 +18,7 @@
     'border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted dark:hover:bg-muted/20',
     className
   )}
+  transition:fade
   {...restProps}
 >
   {@render children?.()}
